a1008de1b0f57cae3f54a43eb9c90368cda67db3
Matt Downey <matthewddowney@gmail.com>
2016-11-12 19:13:48 -0500

    Initial commit

--- RandomGeneratorApp/ViewController.swift
+++ RandomGeneratorApp/ViewController.swift
@@ -7,12 +7,20 @@
 //
 
 import UIKit
+import GameplayKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var lowerBoundField: UITextField!
+    @IBOutlet var upperBoundField: UITextField!
+    @IBOutlet var resultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //lowerBoundField.text = "1"
+        //upperBoundField.text = "100"
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +28,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func generateRandomNum(with min: Int, and max: Int) -> Int {
+        if max < min { return min }
+        let randomNum = GKRandomDistribution(lowestValue: min, highestValue: max)
+        return randomNum.nextInt()
+    }
+    
+    @IBAction func generateRandom() {
+        
+        let minNum = Int(lowerBoundField.text!)
+        let maxNum = Int(upperBoundField.text!)
+        
+        let randomNum = generateRandomNum(with: minNum!, and: maxNum!)
+        
+        resultLabel.text = String(randomNum)
+     }
 }
 
